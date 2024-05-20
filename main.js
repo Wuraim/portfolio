13435a25c312f570bfc510d07da36491dd80643c
Wuraim <lorgnierg@yahoo.com>
2024-05-20 17:42:53 +0200

    avancement

--- main.js
+++ main.js
@@ -4,6 +4,21 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
+document.addEventListener("DOMContentLoaded", function() {
+    function calculateAge(birthdate) {
+        const today = new Date();
+        const birthDate = new Date(birthdate);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDifference = today.getMonth() - birthDate.getMonth();
+        if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
 
+    const ageElement = document.getElementById("age");
+    const birthdate = "1998-07-29";
+    ageElement.textContent = calculateAge(birthdate) + " ans";
+});
 
 setupCounter(document.querySelector('#counter'))
